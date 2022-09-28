cd2e3aba11391a336e1ed71e36cf79b5570f5337
darzzler <darwinphenderson@gmail.com>
2022-09-28 16:50:24 -0700

    upated package and added new scenes

--- src/index.js
+++ src/index.js
@@ -1,39 +1,11 @@
-import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
-
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
-        super();
-    }
-
-    preload ()
-    {
-        this.load.image('logo', logoImg);
-    }
-      
-    create ()
-    {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
+import 'phaser';
+import config from './config/config';
+import GameScene from './scenes/GameScene';
+class Game extends Phaser.Game {
+  constructor () {
+    super(config);
+    this.scene.add('Game', GameScene);
+    this.scene.start('Game');
+  }
 }
-
-const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame
-};
-
-const game = new Phaser.Game(config);
+window.game = new Game();
